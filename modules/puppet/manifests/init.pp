2957019dd59b39c0f5ae324ed182bbeb5cba34f5
binoy <binoybcompany@gmail.com>
2014-02-19 14:46:45 +0530

    automatically pull updates from the git repo & deploying the same / also seeting a cron job to do this periodically

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,24 @@ class puppet {
 	source => 'puppet:///modules/puppet/papply.sh',
 	mode => '0755',
 	}
-}
+
+	file { '/usr/bin/pull-updates':
+	source => 'puppet:///modules/puppet/pull-updates.sh',
+	mode => '0755',
+	}
+	
+	file { '/root/.ssh/id_rsa':
+	source => 'puppet:///modules/puppet/puppetmaster.priv',
+	owner  => 'root',
+	mode   => '0600',
+	}	
+
+	cron { 'run-puppet':
+	ensure => 'present',
+	user   => 'root',
+	command => '/usr/bin/pull-updates',
+	minute  => '*/10',
+	hour    => '*',
+	}
+}	
+	
